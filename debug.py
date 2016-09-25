71935e1a7904d88bff8678e698904158888120f4
StephenChusang <gdgyzzl@163.com>
2016-09-25 10:34:39 +0800

    update

--- debug.py
+++ debug.py
@@ -14,10 +14,11 @@ def debug_vdbc():
         dbpath = None
 
     if gtpath is not None and dbpath is not None:
-        vdbc = VDBC(dbtype=dbtype, dbpath=dbpath, gtpath=gtpath, flush=False)
+        vdbc = VDBC(dbtype=dbtype, dbpath=dbpath, gtpath=gtpath, flush=True)
         image_list, gt_info, folder_map = vdbc.get_db()
-        vdbc.build_data(params=(0.2, 0.2, 0.05, 0.7, 0.5),
-                        num=12)
+        # vdbc.build_data(params=(0.2, 0.2, 0.05, 0.7, 0.5),
+        #                 num=12)
+        print image_list['David']
         print 'End.'
 
 
